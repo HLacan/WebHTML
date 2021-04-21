a57be45f21d37f51e2dc2efb178971d61d5e32b2
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-04-21 14:48:05 -0600

    implementacion de descarga pdf

--- js/doctor.js
+++ js/doctor.js
@@ -1,4 +1,70 @@
+function pdfDoctores() {
+    window.jsPDF = window.jspdf.jsPDF
+    const pdf = new jsPDF();
+
+    fetch('http://127.0.0.1:5000/api/getDoctores')
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log('hubo un problema' + response.status);
+                return;
+            } else {
+                response.json().then(function (data) {
+                    const lista = []
+                    for (var i = 0; i < data.length; i++) {
+                        var doctor = data[i]
+                        var dataDoctor = {
+                            'nombre': doctor.nombre,
+                            'apellido': doctor.apellido,
+                            'fecha': doctor.fecha,
+                            'sexo': doctor.sexo,
+                            'usuario': doctor.usuario,
+                            'contrasena': doctor.contrasena,
+                            'especialidad': doctor.especialidad,
+                            'telefono': doctor.telefono
+                        }        
+                        lista.push(dataDoctor)
+                    }
+                    console.log("mi lista" + JSON.stringify(lista))
+
+                    const header = [['Nombre', 'Apellido', 'Fecha', 'Sexo', 'Usuario', 'Contrasena', 'Especialidad', 'Telefono']];
+                    const rows = [];
+                
+                    lista.forEach(elm => {
+                        const temp = [elm.nombre, elm.apellido, elm.fecha, elm.sexo, elm.usuario, elm.contrasena, elm.especialidad, elm.telefono];
+                        rows.push(temp);
+                    });
+
+                    pdf.text('Listado de Doctores', 80,10,)            
+                    pdf.autoTable({
+                        head: header,
+                        body: rows,
+                    });
+                    pdf.save('ListadoDoctores.pdf');
+                });
+            }
+        }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :(', err);
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+}
 function getDoctores() {
+    var listaDoctor = []
     var htmlTable = document.getElementById("cuerpoTabla")
     fetch('http://127.0.0.1:5000/api/getDoctores')
         .then(function (response) {
@@ -8,8 +74,9 @@ function getDoctores() {
             } else {
                 response.json().then(function (data) {
                     console.log(data);
-                    for(var i = 0; i < data.length; i++){
+                    for (var i = 0; i < data.length; i++) {
                         var doctor = data[i]
+                        var filaDoctor = [doctor.nombre, doctor.apellido, doctor.fecha, doctor.sexo, doctor.usuario, doctor.contrasena, doctor.especialidad, doctor.telefono]
                         htmlTable.innerHTML += `
                         <tr>
                             <td>${doctor.nombre}</td>
@@ -21,14 +88,17 @@ function getDoctores() {
                             <td>${doctor.especialidad}</td>
                             <td>${doctor.telefono}</td>
                         </tr>`
+
+                        listaDoctor.push(filaDoctor)
                     }
+                    console.log(listaDoctor)
                 });
             }
         }
         )
-    .catch(function (err) {
-        console.log('Fetch Error :-S', err);
-    });
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
 }
-
 getDoctores()
+
