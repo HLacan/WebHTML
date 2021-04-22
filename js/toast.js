3d20d15b99c45962bef5fe55912d6ce7df9a1b02
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-04-22 01:35:33 -0600

    mensajes de estado de notificacion (toast)

--- js/toast.js
+++ js/toast.js
@@ -2,10 +2,23 @@
 var option =
 {
     animation: true,
-    delay: 2000
+    delay: 3000
 };
 
-function Toasty() {
+function Toasty(estado) {
+    if(estado == 'repetido'){
+        document.getElementById('toastBody').innerHTML = 'Uno o varios Registros Repetidos'
+    } else if(estado == 'noArchivo'){
+        document.getElementById('toastBody').innerHTML = 'No ay archivo que cargar'
+    } else if(estado == 'modificar'){
+        document.getElementById('toastBody').innerHTML = 'Registro Modificado'
+    } else if(estado == 'eliminar'){
+        document.getElementById('toastBody').innerHTML = 'Registro Eliminado'
+    } else if(estado == 'reporte'){
+        document.getElementById('toastBody').innerHTML = 'Reporte Generado!'
+    }
+
+
     var toastHTMLElement = document.getElementById('EpicToast');
 
     var toastElement = new bootstrap.Toast(toastHTMLElement, option);
