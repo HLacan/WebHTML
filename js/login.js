749007d1403ba34731d4b72516c6b60e01093fa7
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-01 09:01:18 -0600

    verificacion de admin y doctor

---
+++ js/login.js
@@ -0,0 +1,32 @@
+function login() {
+    usuario = document.getElementById('usuario').value
+    contrasena = document.getElementById('contrasena').value
+
+    if (usuario.trim() != "" || contrasena.trim() != ""){
+        console.log(usuario)
+        console.log(contrasena)
+    
+        fetch('http://127.0.0.1:5000/api/login', {
+            method: 'post',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                'usuario': usuario,
+                'contrasena': contrasena,
+            })
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            console.log(jsonResponse);
+            if (jsonResponse['res'] == 'admin'){
+                console.log('amonos a admin page')
+                document.location.href = "../html/inicioAdmin.html"
+            } else if (jsonResponse['res'] == 'doctor'){
+                console.log('amonos a doctor page')
+            }
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+
+}
