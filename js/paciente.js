74b106c088d1cd255c585c0d0578db0fc076958b
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-02 17:18:18 -0600

    metodo get y update de paciente

--- js/paciente.js
+++ js/paciente.js
@@ -20,7 +20,7 @@ function getPacientes() {
                     <td>${paciente.telefono}</td>
                     <td>
                         <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modificar" onclick="getPaciente('${paciente.usuario}')" type="submit"><i class="fa fa-pencil" style="font-size:15px; color:white;"></i></button>
-                        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#eliminarModal" onclick="getEliminarDoctor('${paciente.usuario}')" type="submit"><i class="fa fa-trash"></i></button>
+                        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#eliminar" onclick="opcionesDelete('${paciente.usuario}')" type="submit"><i class="fa fa-trash"></i></button>
                     </td>
                 </tr>`
                 listaPacientes.push(filaPaciente)
@@ -141,6 +141,119 @@ function pdfPacientes() {
 
 
 
+}
+
+function getPaciente(usuario) {
+    selectIndice = 0
+    fetch(`http://127.0.0.1:5000/api/getPaciente/${usuario}`)
+        .then((resp) => resp.json(
+        )).then(function (response) {
+            if (response.genero == 'M') {
+                selectIndice = 0
+            } else if (response.genero == 'F') {
+                selectIndice = 1
+            }
+
+            console.log(response)
+            var fecha = (response.fecha)
+            var newFecha = fecha.split('/')
+            document.getElementById('nombre').value = response.nombre
+            document.getElementById('apellido').value = response.apellido
+            document.getElementById('fecha').value = newFecha[2] + '-' + newFecha[1] + '-' + newFecha[0]
+            document.getElementById('genero').selectedIndex = selectIndice
+            document.getElementById('usuario').value = response.usuario
+            document.getElementById('contrasena').value = response.contrasena
+            document.getElementById('telefono').value = response.telefono
+            document.getElementById('footerModal').innerHTML = `
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+            <button type="button" class="btn btn-primary" onclick="validarPaciente('${response.usuario}')">Modificar</button>`
+        }).catch(function (error) {
+            console.log(error);
+        });
+}
+
+function validarPaciente(oldUsuario) {
+    newUsuario = document.getElementById('usuario').value
+    if (oldUsuario != newUsuario) {
+        fetch(`http://127.0.0.1:5000/api/validar/${oldUsuario}/${newUsuario}`)
+            .then((resp) => resp.json())
+            .then(function (response) {
+                console.log(response)
+                if (response['res'] == 'libre') {
+                    updatePaciente(oldUsuario, newUsuario)
+                    cerrarModal('#modificar')
+                    Toasty('modificar')
+                } else {
+                    console.log('el nombre de usuario le pertenece a otra persona')
+                    Toasty('existe')
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    } else if (oldUsuario == newUsuario) {
+        console.log('modificando el mismo usuario')
+        updateEnfermera(oldUsuario, newUsuario)
+        cerrarModal('#modificar')
+        Toasty('modificar')
+    }
+}
+
+function updatePaciente(oldUsuario, newUsuario) {
+    nombre = document.getElementById('nombre').value
+    apellido = document.getElementById('apellido').value
+    oldFecha = document.getElementById('fecha').value
+    genero = document.getElementById('genero').value
+    contrasena = document.getElementById('contrasena').value
+    telefono = document.getElementById('telefono').value
+    splittedFecha = oldFecha.split('-')
+    newFecha = splittedFecha[2] + '/' + splittedFecha[1] + '/' + splittedFecha[0]
+
+    fetch('http://127.0.0.1:5000/api/updatePaciente', {
+        method: 'post',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+            'nombre': nombre,
+            'apellido': apellido,
+            'fecha': newFecha,
+            'genero': genero,
+            'oldUsuario': oldUsuario,
+            'newUsuario': newUsuario,
+            'contrasena': contrasena,
+            'telefono': telefono
+        })
+    }).then(response => {
+        return response.json();
+    }).then(jsonResponse => {
+        console.log(jsonResponse)
+        getPacientes()
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+function opcionesDelete(usuario) {
+    document.getElementById('eliminarFooter').innerHTML = `
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+    <button type="button" class="btn btn-primary" onclick="deletePaciente('${usuario}')">Eliminar</button>`
+}
+
+function deletePaciente(usuario) {
+    fetch(`http://127.0.0.1:5000/api/deletePaciente/${usuario}`)
+        .then((resp) => resp.json())
+        .then(function (response) {
+            console.log(response)
+            cerrarModal('#eliminar')
+            Toasty('eliminar')
+            getPacientes()
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function cerrarModal(modal) {
+    $(modal).modal('hide');
 }
 
 getPacientes()
