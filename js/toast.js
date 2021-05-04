01fb4fafd252662be432429996ab2371b6aa5ef7
Herbert Rafael Lacan Hernandez <3017517650101@ingenieria.usac.edu.gt>
2021-05-03 22:02:23 -0600

    mensajes de notifiacion

--- js/toast.js
+++ js/toast.js
@@ -7,27 +7,31 @@ var option =
 
 function Toasty(estado) {
     toast = document.getElementById('toastBody')
-    if(estado == 'repetido'){
+    if (estado == 'repetido') {
         toast.innerHTML = 'Uno o varios Registros Repetidos'
-    } else if(estado == 'noArchivo'){
+    } else if (estado == 'noArchivo') {
         toast.innerHTML = 'No ay archivo que cargar'
-    } else if(estado == 'modificar'){
+    } else if (estado == 'modificar') {
         toast.innerHTML = 'Registro Modificado'
-    } else if(estado == 'eliminar'){
+    } else if (estado == 'eliminar') {
         toast.innerHTML = 'Registro Eliminado'
-    } else if(estado == 'reporte'){
+    } else if (estado == 'reporte') {
         toast.innerHTML = 'Reporte Generado!'
-    } else if(estado == 'existe'){
+    } else if (estado == 'existe') {
         toast.innerHTML = 'El usuario ya existe!'
-    } else if(estado == 'citaA'){
+    } else if (estado == 'citaA') {
         toast.innerHTML = 'Cita Agregada!'
-    } else if(estado == 'citaP'){
+    } else if (estado == 'citaP') {
         toast.innerHTML = 'Ya hay una cita Pendiente!'
-    } else if(estado == 'aceptado'){
+    } else if (estado == 'aceptado') {
         toast.innerHTML = 'Cita Aceptada!'
-    } else if(estado == 'rechazado'){
+    } else if (estado == 'rechazado') {
         toast.innerHTML = 'Cita Rechazada!'
-    } 
+    } else if (estado == 'agregado') {
+        toast.innerHTML = 'PacienteAgregado!'
+    } else if (estado == 'no existe') {
+        toast.innerHTML = 'usuario o contrasena incorrectos!'
+    }
 
 
     var toastHTMLElement = document.getElementById('EpicToast');
